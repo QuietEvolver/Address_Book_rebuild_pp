6531c03a28e4c3a377682b954ef3e24539afff14
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-10-31 10:32:54 -0700

    It will call the method to assign id for contacts param through it's value:   addressBook.addContact(contacts).
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>.

--- js/scripts.js
+++ js/scripts.js
@@ -42,15 +42,9 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
 
-  let addressBook = new AddressBook(); // instance ofAB
-  console.log("addressBook instance", addressBook);
-  // // let contacts = new Contact(); // create any contact instance(s) for the AB
-  // console.log("contact instance: ", contacts);
-  let addressContacts = [];
-  // addressBook.push(contacts); // push contact into AB
-  
-  
-  // UI Logic
+  // let addressContactsStrToArr = addressBook.split(contacts); // push contact into AB
+  // console.log("addressContactsStrToArr: ", addressContactsStrToArr);
+  // // UI Logic
 
 function handleFormSubmission(e){
   e.preventDefault();
@@ -60,9 +54,16 @@ function handleFormSubmission(e){
   console.log("First Name: ", firstName);
   console.log("last Name: ", lastName);
   console.log("Phone: ", phoneNumber);
-  console.log("addressBook.contacts: ", addressBook.contacts);
-  let contacts = new Contact(firstName, lastName, phoneNumber); // must pass in corresponding params in order for fx to run
+  // instance ofAB
+  let addressBook = new AddressBook(); 
+  // must pass in corresponding params in order for fx to run
+  // create any contact instance(s) for the AB
+  console.log("addressBook instance", addressBook);
+  let contacts = new Contact(firstName, lastName, phoneNumber); 
   console.log("contact instance: ", contacts);
+  console.log("addressBook.contacts: ", addressBook.contacts);
+  addressBook.addContact(contacts);
+  
   document.getElementById("output").innerText = addressContacts;
 }
 
